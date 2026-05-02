e96f44135808f23e81a8ee85a20ea7dea6ae5e7b
GurthBro0ks <whoompagee@gmail.com>
2026-05-02 12:07:01 +0000

    fix(web): remove build error suppression flags

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'media.discordapp.net', pathname: '/**' },
     ],
   },
-  typescript: { ignoreBuildErrors: true },
+  typescript: { ignoreBuildErrors: false },
 };
 
 module.exports = nextConfig;
